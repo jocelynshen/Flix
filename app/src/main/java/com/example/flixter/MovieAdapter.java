08cb11c9ad33ea75996798daeed078d525ab2d7e
jocelynshen <jocelyn.j.shen@gmail.com>
2019-06-26 17:48:05 -0700

    alternate layout

--- app/src/main/java/com/example/flixter/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixter;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,12 +39,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
         ImageView ivPosterImage;
+        ImageView ivBackdropImage;
         TextView tvTitle;
         TextView tvOverview;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ivPosterImage = itemView.findViewById(R.id.ivPosterImage);
+            ivBackdropImage = itemView.findViewById(R.id.ivBackdropImage);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
         }
@@ -65,13 +68,22 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         Movie movie = movies.get(position);
         holder.tvTitle.setText(movie.getTitle());
         holder.tvOverview.setText(movie.getOverview());
-        String imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        boolean isPortrait = context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        String imageUrl = null;
+        if (isPortrait){
+            imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        } else {
+            imageUrl = config.getImageUrl(config.getBackdropSize(), movie.getBackdropPath());
+        }
+
+        int placeholderId = isPortrait ? R.drawable.flicks_movie_placeholder: R.drawable.flicks_backdrop_placeholder;
+        ImageView imageView = isPortrait ? holder.ivPosterImage: holder.ivBackdropImage;
         Glide.with(context)
                 .load(imageUrl)
                 .bitmapTransform(new RoundedCornersTransformation(context, 15, 0))
-                .placeholder(R.drawable.flicks_movie_placeholder)
-                .error(R.drawable.flicks_movie_placeholder)
-                .into(holder.ivPosterImage);
+                .placeholder(placeholderId)
+                .error(placeholderId)
+                .into(imageView);
 
     }
 
--- app/src/main/java/com/example/flixter/models/Config.java
+++ app/src/main/java/com/example/flixter/models/Config.java
@@ -7,12 +7,20 @@ import org.json.JSONObject;
 public class Config {
     String imageBaseUrl;
     String posterSize;
+    String backdropSize;
 
     public Config(JSONObject object) throws JSONException {
         JSONObject images = object.getJSONObject("images");
         imageBaseUrl = images.getString("secure_base_url");
         JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         posterSize = posterSizeOptions.optString(3, "w342");
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
+
+    }
+
+    public String getBackdropSize() {
+        return backdropSize;
     }
 
     public String getImageUrl(String size, String path){
